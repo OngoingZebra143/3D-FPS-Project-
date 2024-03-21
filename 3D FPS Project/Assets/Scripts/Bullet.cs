eab2117726c486a1f44762e11e956f19cf60e45f
OngoingZebra143 <146136142+OngoingZebra143@users.noreply.github.com>
2024-03-21 13:51:16 -0700

    updating

--- 3D FPS Project/Assets/Scripts/Bullet.cs
+++ 3D FPS Project/Assets/Scripts/Bullet.cs
@@ -19,4 +19,23 @@ public class Bullet : MonoBehaviour
     {
         _rigidbody.velocity = transform.forward * moveSpeed; 
     }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("TargetStanding"))
+        {
+            Debug.Log("I hit the standing target.");
+            //add code to add hit points to your scoreboard
+            //gray out the standing target
+            other.gameObject.SetActive(false);
+        }
+
+        if(other.gameObject.CompareTag("TargetFloating"))
+        {
+            Debug.Log("I hit the floating target");
+            //add code to add hit points to ur scoreboard
+            Destroy(this.gameObject);
+            Destroy(other.gameObject);
+        }
+    }
 }
